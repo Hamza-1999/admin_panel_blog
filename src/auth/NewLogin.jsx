541c03b797ba19a3fedadc7c5933d9c6284be697
Harmain Khan <harmainkhan879@gmail.com>
2023-10-09 06:00:04 -0700

    working on login

---
+++ src/auth/NewLogin.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./newlogin.css";
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Grid,
+    InputLabel,
+    Stack,
+    TextField,
+    Typography,
+  } from "@mui/material";
+
+
+
+const NewLogin = () => {
+  return (
+    <>
+      <Box className="login" >
+
+        <Stack direction="row" id="inner" variant="mt-3" >
+            <form>
+                
+            </form>
+        </Stack>
+      </Box>
+      
+    </>
+  )
+}
+
+export default NewLogin;
